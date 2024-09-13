01df9d4c866505de99b467c7e9306527f1829a6f
Delan Azabani <delan@azabani.com>
2024-09-13 21:51:36 +0800

    dom: enforce single <html> root invariant when serializing

--- src/dom.rs
+++ src/dom.rs
@@ -1,13 +1,14 @@
 use std::{borrow::Borrow, str};
 
 use html5ever::{
+    interface::{ElementFlags, TreeSink},
     local_name, namespace_url, ns,
     tendril::{StrTendril, TendrilSink},
     tree_builder::TreeBuilderOpts,
     Attribute, LocalName, Namespace, ParseOpts, QualName,
 };
-use jane_eyre::eyre;
-use markup5ever_rcdom::{Handle, RcDom, SerializableHandle};
+use jane_eyre::eyre::{self, bail};
+use markup5ever_rcdom::{Handle, NodeData, RcDom, SerializableHandle};
 
 pub struct Traverse(Vec<Handle>);
 
@@ -52,12 +53,30 @@ pub fn parse(mut input: &[u8]) -> eyre::Result<RcDom> {
 
 pub fn serialize(dom: RcDom) -> eyre::Result<String> {
     // html5ever::parse_fragment builds a tree with the input wrapped in an <html> element.
-    let html_root: SerializableHandle = dom.document.children.borrow()[0].clone().into();
+    // this is consistent with how the web platform dom requires exactly one root element.
+    let children = dom.document.children.borrow();
+    if children.len() != 1 {
+        bail!(
+            "expected exactly one root element but got {}",
+            children.len()
+        );
+    }
+    let html = QualName::new(None, ns!(html), local_name!("html"));
+    if !matches!(&children[0].data, NodeData::Element { name, .. } if name == &html) {
+        bail!("expected root element to be <html>");
+    }
+    let html_root: SerializableHandle = children[0].clone().into();
+
     let mut result = Vec::default();
     html5ever::serialize(&mut result, &html_root, Default::default())?;
     let result = String::from_utf8(result)?;
 
     Ok(result)
+}
+
+pub fn create_element(dom: &mut RcDom, html_local_name: &str) -> Handle {
+    let name = QualName::new(None, ns!(html), LocalName::from(html_local_name));
+    dom.create_element(name, vec![], ElementFlags::default())
 }
 
 pub fn find_attr_mut<'attrs>(
@@ -88,4 +107,28 @@ pub fn attr_value<'attrs>(
 
 pub fn tendril_to_str(tendril: &StrTendril) -> eyre::Result<&str> {
     Ok(str::from_utf8(tendril.borrow())?)
+}
+
+#[test]
+fn test_serialize() -> eyre::Result<()> {
+    assert_eq!(serialize(RcDom::default()).map_err(|_| ()), Err(()));
+
+    let mut dom = RcDom::default();
+    let html = create_element(&mut dom, "html");
+    dom.document.children.borrow_mut().push(html);
+    assert_eq!(serialize(dom)?, "");
+
+    let mut dom = RcDom::default();
+    let html = create_element(&mut dom, "html");
+    dom.document.children.borrow_mut().push(html);
+    let html = create_element(&mut dom, "html");
+    dom.document.children.borrow_mut().push(html);
+    assert_eq!(serialize(dom).map_err(|_| ()), Err(()));
+
+    let mut dom = RcDom::default();
+    let html = create_element(&mut dom, "p");
+    dom.document.children.borrow_mut().push(html);
+    assert_eq!(serialize(dom).map_err(|_| ()), Err(()));
+
+    Ok(())
 }
