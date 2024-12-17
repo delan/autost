8ffec78de2e2556de7c9781192c77924d8eb02b0
Ruby Iris Juric <ruby@srxl.me>
2024-12-17 20:57:09 +1100

    nix: initial flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "cohost-compatible blog engine and feed reader";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
