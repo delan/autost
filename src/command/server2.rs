1e9033167696f027d0c5bd1e3247d9d4918060cf
Aria Edmonds <aria@ar1as.space>
2025-03-03 22:59:04 +0800

    server: add publish route

--- src/command/server2.rs
+++ src/command/server2.rs
@@ -1,18 +1,25 @@
+use std::{
+    fs::File,
+    io::{self, Write as _},
+};
+
 use crate::{
-    command::render::render_all, output::ThreadsContentTemplate, path::POSTS_PATH_ROOT,
+    command::render::render_all,
+    output::ThreadsContentTemplate,
+    path::{PostsPath, POSTS_PATH_ROOT},
     render_markdown, Command, PostMeta, TemplatedPost, Thread, SETTINGS,
 };
 
 use askama_rocket::Template;
 use chrono::{SecondsFormat, Utc};
 use clap::Parser as _;
-use jane_eyre::eyre::Context;
+use jane_eyre::eyre::{Context, OptionExt as _};
 use rocket::{
     form::Form,
     fs::{FileServer, Options},
     get, post,
     response::{content, Redirect},
-    routes, Config, FromForm,
+    routes, Config, FromForm, Responder,
 };
 use rocket_errors::eyre;
 
@@ -76,6 +83,52 @@ fn preview_route(body: Form<Body<'_>>) -> eyre::Result<content::RawHtml<String>>
     ))
 }
 
+#[derive(Responder)]
+enum Content {
+    Redirect(Box<Redirect>),
+    Text(String),
+}
+
+#[post("/publish?<js>", data = "<body>")]
+fn publish_route(js: Option<bool>, body: Form<Body<'_>>) -> eyre::Result<Content> {
+    let js = js.unwrap_or_default();
+    let unsafe_source = body.source;
+
+    // try rendering the post before writing it, to catch any errors.
+    let unsafe_html = render_markdown(unsafe_source);
+    let post = TemplatedPost::filter(&unsafe_html, None)?;
+    let _thread = Thread::try_from(post)?;
+
+    // cohost post ids are all less than 10000000.
+    let (mut file, path) = (10000000..)
+        .map(|id| {
+            let path = PostsPath::markdown_post_path(id);
+            File::create_new(&path).map(|file| (file, path))
+        })
+        .find(|file| !matches!(file, Err(error) if error.kind() == io::ErrorKind::AlreadyExists))
+        .expect("too many posts :(")
+        .wrap_err("failed to create post")?;
+
+    file.write_all(unsafe_source.as_bytes())
+        .wrap_err("failed to write post file")?;
+    render_all()?;
+
+    let post = TemplatedPost::load(&path)?;
+    let _thread = Thread::try_from(post)?;
+    let url = path
+        .rendered_path()?
+        .ok_or_eyre("path has no rendered path")?
+        .internal_url();
+
+    // fetch api does not expose the redirect ‘location’ to scripts.
+    // <https://github.com/whatwg/fetch/issues/763>
+    if js {
+        Ok(Content::Text(url))
+    } else {
+        Ok(Content::Redirect(Box::new(Redirect::to(url))))
+    }
+}
+
 // lower than FileServer, which uses rank 10 by default
 #[get("/", rank = 100)]
 fn root_route() -> Redirect {
@@ -101,7 +154,10 @@ pub async fn main() -> jane_eyre::eyre::Result<()> {
 
     let port = args.port.unwrap_or(SETTINGS.server_port());
     let _rocket = rocket::custom(Config::figment().merge(("port", port)))
-        .mount(&SETTINGS.base_url, routes![compose_route, preview_route])
+        .mount(
+            &SETTINGS.base_url,
+            routes![compose_route, preview_route, publish_route],
+        )
         .mount("/", routes![root_route])
         .mount(
             &SETTINGS.base_url,
