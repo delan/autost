809f900311518b93c9663cf634d61281e8273fd2
Delan Azabani <delan@azabani.com>
2025-08-14 18:52:12 +0800

    create a per-site sqlite database

--- build.rs
+++ build.rs
@@ -5,8 +5,10 @@ use vergen_gix::{Emitter, GixBuilder};
 fn main() -> Result<(), Box<dyn Error>> {
     let mut gix = GixBuilder::default();
     gix.describe(false, true, None);
-
     Emitter::default().add_instructions(&gix.build()?)?.emit()?;
+
+    // sqlx migrations (see src/migrations.rs)
+    println!("cargo:rerun-if-changed=migrations");
 
     Ok(())
 }
